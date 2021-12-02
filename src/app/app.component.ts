bb7737d4a9d71d4e816f3d489f78730be1b7f008
kotekirkitadze <kotekirkitadze@gmail.com>
2021-12-03 00:39:00 +0400

    structure needs cleaining\

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TreeNodeService } from './tree-node.service';
+
+export interface TreeNode {
+  data?: any;
+  children?: TreeNode[];
+  leaf?: boolean;
+  expanded?: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'testTreeTableApp';
+export class AppComponent implements OnInit {
+  files: TreeNode[];
+
+  cols: any[];
+
+  totalRecords: number;
+
+  loading: boolean;
+
+  constructor(private getDataService: TreeNodeService) {}
+  ngOnInit() {
+    // this.getDataService.getData().subscribe((d) => (this.files = d));
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' },
+    ];
+
+    //in a production application, retrieve the logical number of rows from a remote datasource
+    this.totalRecords = 1000;
+
+    this.loading = true;
+  }
+  editDoc: boolean = false;
+  loadNodes(event) {
+    this.loading = true;
+
+    this.getDataService.getPackages().subscribe((d) => {
+      this.files = d
+        .filter((e) => e.data['userNum'] >= 1)
+        .map((d) => {
+          return {
+            ...d,
+            leaf: false,
+          };
+        });
+      this.loading = false;
+    });
+  }
+
+  onNodeExpand(event) {
+    this.loading = true;
+
+    console.log(event);
+
+    let node = event.node;
+    console.log(node.data['PackageId']);
+
+    console.log(node.data['type']);
+    if (event.node.data['type'] == 'paketebi') {
+      this.getDataService
+        .getUsers(node.data['PackageId'])
+        .subscribe((users) => {
+          node.children = users
+            .filter((user) => user.data['documents'] >= 1)
+            .map((user) => {
+              return {
+                ...user,
+                leaf: false,
+              };
+            });
+          this.loading = false;
+          this.files = [...this.files];
+        });
+    }
+
+    if (node.data['type'] == 'usersType') {
+      this.getDataService.getDocs(node.data['userID']).subscribe((docs) => {
+        node.children = docs;
+        this.loading = false;
+        this.editDoc = true;
+        this.files = [...this.files];
+      });
+    }
+  }
+
+  edit(e) {
+    console.log('Document name:', e.name);
+    console.log('Document"s userId', e.useID);
+  }
 }
---
+++ src/app/tree-node.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { TreeNode } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TreeNodeService {
+  constructor(private http: HttpClient) {}
+
+  urlPackages: string = 'http://localhost:5000/packages';
+  urlUsers: string = 'http://localhost:5000/users';
+  urlDocs: string = 'http://localhost:5000/documents';
+
+  getPackages(): Observable<TreeNode[]> {
+    return this.http.get<TreeNode[]>(this.urlPackages);
+  }
+
+  getUsers(packId): Observable<TreeNode[]> {
+    return this.http
+      .get<TreeNode[]>(this.urlUsers)
+      .pipe(map((el) => el.filter((u) => u.data.PackageId === packId)));
+  }
+
+  getDocs(id): Observable<TreeNode[]> {
+    return this.http
+      .get<TreeNode[]>(this.urlDocs)
+      .pipe(map((el) => el.filter((d) => d.data.useID === id)));
+  }
+}
